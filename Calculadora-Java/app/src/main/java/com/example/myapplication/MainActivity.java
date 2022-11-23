ab7ffe99def0f1267deb3a3535193ad812ac7b90
HumbertoCG18 <humberto.cgomes17@gmail.com>
2022-11-23 09:26:07 -0300

    Codes and Design Java Android

---
+++ Calculadora-Java/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnSoma, btnSubtracao;
+    Integer textValor1, textValor2, Soma, Subtracao;
+    EditText textResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnSoma = findViewById(R.id.btnSoma);
+        btnSoma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                Soma = 1 + 1;
+                EditText textResultado = findViewById(R.id.txtResultado);
+                textResultado.setText(Soma);
+            }
+        });
+
+        Button btnSubtracao = findViewById(R.id.btnSubtracao);
+        btnSubtracao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                
+                Subtracao = 1 - 1;
+                EditText textResultado = findViewById(R.id.txtResultado);
+                textResultado.setText(Subtracao);
+            }
+        });
+    }
+}
